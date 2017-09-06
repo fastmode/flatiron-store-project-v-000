0f20bcebbd1abe2ef45f645a7d763890c04d09f3
Art Rodriguez <art.rodriguez@live.com>
2017-09-06 02:38:05 -0400

    Model specs passing tests

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,19 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
+  belongs_to :user
+
+  def total
+    self.line_items.collect { |line_item| line_item.item.price * line_item.quantity }.sum
+  end 
+
+  def add_item(new_item)
+    if line_item = self.line_items.find_by(item_id: new_item)
+      line_item.update(quantity: line_item.quantity + 1)
+      line_item 
+    else      
+      self.line_items.build(item_id: new_item)
+    end
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe User, type: :model do
   end
 
   it "has a current_cart" do
+    binding.pry
+    
     @user.current_cart = @user.carts.create
     expect(@user.current_cart).to be_a(Cart)
   end
